1f7296d27f062e7409e7bf58c5f7f5afbaf6e3a9
syed-hamed-raza <100027534+syed-hamed-raza@users.noreply.github.com>
2022-03-23 09:43:41 +0500

    code

--- STAT545/DataAnalysis01.R
+++
@@ -1,8 +0,0 @@
-install.packages("gapminder")
-library(gapminder)
-str(gapminder)
-install.packages("tidyverse")
-library(tidyverse)
-class(gapminder)
-gapminder
-write.csv(gapminder,"gapminder.csv",row.names = F)
